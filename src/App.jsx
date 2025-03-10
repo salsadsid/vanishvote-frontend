963ee327cb398936ae08e0234f4ef4f999261338
Salman Sadik Siddiquee <salman.dnj@gmail.com>
2025-03-10 23:23:52 +0600

    project initailization, vite-react project setup, tailwind integration, remove boilerplate code

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl text-purple-700">Vanish Vote</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
